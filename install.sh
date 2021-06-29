7e5bf03536c63050f24dcd387249e0956bd97b44
choimake <52233450+choimake@users.noreply.github.com>
2021-06-29 22:40:07 +0900

    brewのパスを通す処理を追加 (#14)

--- install.sh
+++ install.sh
@@ -8,6 +8,10 @@ if !(type 'brew' > /dev/null 2>&1); then
   # Install brew command.
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+  # Add brew path.
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+
   # If install brew failed, exit this script.
   if !(type 'brew' > /dev/null 2>&1); then
     echo 'Fail to install brew.'
