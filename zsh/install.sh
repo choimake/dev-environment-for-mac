79b95de280c32331658d79ff56a80867cc0c8b68
choimake <52233450+choimake@users.noreply.github.com>
2021-08-29 16:14:58 +0900

    zinitとプラグイン導入 (#62)

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
+# zinit install
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
 
+# copy zshrc
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
 ln -sf $SCRIPT_DIR/.zshrc ~/.zshrc
+
+# load zshrc
+source ~/.zshrc
